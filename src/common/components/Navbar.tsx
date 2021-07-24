23c92c27e9eecacf19ccac0151cdd5b1d0ec042e
Duc Filan <ducfilan@gmail.com>
2021-07-24 16:54:35 +0800

    Add Navbar component

--- src/common/components/Navbar.tsx
+++ src/common/components/Navbar.tsx
@@ -1,7 +1,40 @@
 import * as React from "react"
 
-function Navbar() {
-  return ""
+import { Navbar as BulmaNavbar, Image } from "react-bulma-components"
+
+import logo from "../../images/ui/logo.png"
+
+const { Brand, Item, Menu } = BulmaNavbar
+
+function Navbar({ isLoggedIn }: any) {
+  return (
+    <BulmaNavbar color="primary">
+      <Brand>
+        <figure className="image is-48x48 logo">
+          <Image src={logo} rounded />
+        </figure>
+        <Item renderAs="h4" className="title is-4 is-marginless has-text-centered is-overlay brand-name is-uppercase">
+          {chrome.i18n.getMessage("appName")}
+        </Item>
+
+        <a className="navbar-burger" role="button" data-target="navMenu" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </Brand>
+
+      {isLoggedIn && (
+        <Menu id="navMenu">
+          <div className="navbar-end">
+            <Item className="is-capitalized" id="button-logout" href="#">
+              {chrome.i18n.getMessage("logout")}
+            </Item>
+          </div>
+        </Menu>
+      )}
+    </BulmaNavbar>
+  )
 }
 
 export default Navbar
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,10 +1,12 @@
 import * as React from "react"
 import "./popup.scss"
 
+import Navbar from "../../common/components/Navbar"
+
 const PopupPage = () => {
   return (
     <div className="App">
-      Test
+      <Navbar isLoggedIn></Navbar>
     </div>
   )
 }
