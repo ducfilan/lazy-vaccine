2810a10bef4d1d85a67572940e698b75f8f58b74
Duc Filan <ducfilan@gmail.com>
2021-08-01 10:04:35 +0800

    Navbar component

--- src/common/components/Navbar.tsx
+++ src/common/components/Navbar.tsx
@@ -1,39 +1,50 @@
 import * as React from "react"
 
-import { Navbar as BulmaNavbar, Image } from "react-bulma-components"
+import { PageHeader, Menu, Dropdown, Button } from "antd"
+import { MenuOutlined } from "@ant-design/icons"
 
 import logo from "../../images/ui/logo.png"
 
-const { Brand, Item, Menu } = BulmaNavbar
-
-function Navbar({ isLoggedIn }: any) {
-  return (
-    <BulmaNavbar color="primary">
-      <Brand>
-        <figure className="image is-48x48 logo">
-          <Image src={logo} rounded />
-        </figure>
-        <Item renderAs="h4" className="title is-4 is-marginless has-text-centered is-overlay brand-name is-uppercase">
-          {chrome.i18n.getMessage("appName")}
-        </Item>
-
-        <a className="navbar-burger" role="button" data-target="navMenu" aria-label="menu" aria-expanded="false">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </Brand>
-
-      {isLoggedIn && (
-        <Menu id="navMenu">
-          <div className="navbar-end">
-            <Item className="is-capitalized" id="button-logout" href="#">
+const DropdownMenu = (props: { isLoggedIn: boolean }) => (
+  <Dropdown
+    overlay={
+      <Menu>
+        {props.isLoggedIn && (
+          <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
               {chrome.i18n.getMessage("logout")}
-            </Item>
-          </div>
-        </Menu>
-      )}
-    </BulmaNavbar>
+            </a>
+          </Menu.Item>
+        )}
+      </Menu>
+    }
+    overlayStyle={{
+      minWidth: 120,
+    }}
+  >
+    <Button
+      type="text"
+      style={{
+        border: "none",
+        paddingRight: 8,
+      }}
+    >
+      <MenuOutlined
+        style={{
+          color: "white",
+        }}
+      />
+    </Button>
+  </Dropdown>
+)
+
+function Navbar(props: { isLoggedIn: boolean }) {
+  return (
+    <PageHeader
+      title={chrome.i18n.getMessage("appName")}
+      avatar={{ gap: 0, src: logo, size: 48 }}
+      extra={[<DropdownMenu isLoggedIn={props.isLoggedIn} />]}
+    />
   )
 }
 
