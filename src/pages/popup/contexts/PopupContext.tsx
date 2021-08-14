d453c1a08f2927ad8fee7e6b043ec565d2cea21c
Duc Filan <ducfilan@gmail.com>
2021-08-14 09:36:44 +0800

    Sup: Popup context

--- src/pages/popup/contexts/PopupContext.tsx
+++ src/pages/popup/contexts/PopupContext.tsx
@@ -1,12 +1,18 @@
 import { createContext, useContext } from "react"
-import Language from "../types/Language"
-export type GlobalContent = {
+import { Language, User } from "@/common/types/types"
+
+export type Context = {
   selectedLanguages: Language[]
   setSelectedLanguages: (languages: Language[]) => void
+  user: User | null
+  setUser: (user: User | null) => void
 }
-export const PopupContext = createContext<GlobalContent>({
+
+export const PopupContext = createContext<Context>({
   selectedLanguages: [],
   setSelectedLanguages: () => {},
+  user: null,
+  setUser: () => {},
 })
 
 export const usePopupContext = () => useContext(PopupContext)
