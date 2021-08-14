799024e5c4ddf9ef8923d3421c7cdffd1e800e47
Duc Filan <ducfilan@gmail.com>
2021-08-14 09:24:59 +0800

    Fix: Navbar component

--- src/common/components/Navbar.tsx
+++ src/common/components/Navbar.tsx
@@ -3,17 +3,15 @@ import * as React from "react"
 import { PageHeader, Menu, Dropdown, Button } from "antd"
 import { MenuOutlined } from "@ant-design/icons"
 
-import logo from "../../images/ui/logo.png"
+import logo from "@img/ui/logo.png"
 
 const DropdownMenu = (props: { isLoggedIn: boolean }) => (
   <Dropdown
     overlay={
       <Menu>
         {props.isLoggedIn && (
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
-              {chrome.i18n.getMessage("logout")}
-            </a>
+          <Menu.Item key="logout">
+            <Button type="link">{chrome.i18n.getMessage("logout")}</Button>
           </Menu.Item>
         )}
       </Menu>
@@ -43,7 +41,7 @@ function Navbar(props: { isLoggedIn: boolean }) {
     <PageHeader
       title={chrome.i18n.getMessage("appName")}
       avatar={{ gap: 0, src: logo, size: 48 }}
-      extra={[<DropdownMenu isLoggedIn={props.isLoggedIn} />]}
+      extra={[<DropdownMenu key="menu" isLoggedIn={props.isLoggedIn} />]}
     />
   )
 }
