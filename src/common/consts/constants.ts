071f3d325bf2549651ac3171032fb00f722dd1f2
Duc Filan <ducfilan@gmail.com>
2021-08-14 17:55:03 +0800

    Sup: Remove fixed width/height from svg files

--- src/common/consts/constants.ts
+++ src/common/consts/constants.ts
@@ -1,29 +1,32 @@
-export default {
-  Pages: {
-    NewSet: {
-      fileName: "create-set.html"
-    },
-    NewSetItems: {
-      fileName: "create-set-items.html"
-    },
-    SetsList: {
-      fileName: "sets-list.html"
-    },
-    SetsDetail: {
-      fileName: "set-detail.html"
-    }
+export const Pages = {
+  NewSet: {
+    fileName: "create-set.html"
   },
-  Inject: {
-    Type: {
-      FixedPosition: 1,
-      DynamicGenerated: 2
-    },
-    Rate: {
-
-    }
+  NewSetItems: {
+    fileName: "create-set-items.html"
   },
-  loginTypes: {
-    google: "google"
+  SetsList: {
+    fileName: "sets-list.html"
   },
-  ApiTimeOut: 10000, // milliseconds
+  SetsDetail: {
+    fileName: "set-detail.html"
+  }
 }
+
+export const Inject = {
+  Type: {
+    FixedPosition: 1,
+    DynamicGenerated: 2
+  },
+  Rate: {
+
+  }
+}
+
+export const LoginTypes = {
+  google: "google"
+}
+
+export const ApiTimeOut = 10000 // milliseconds
+
+export const DefaultLangCode = "en"
