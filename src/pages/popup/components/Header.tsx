d1dd2f1f3f574a96e691a27ab9a2570f9540e85d
Duc Filan <ducfilan@gmail.com>
2021-08-24 12:13:11 +0800

    Feat: Fix header content type

--- src/pages/popup/components/Header.tsx
+++ src/pages/popup/components/Header.tsx
@@ -2,14 +2,18 @@ import * as React from "react"
 
 import { Typography } from "antd"
 
-function PopupHeader({ content, iconUrl }: { content: string | React.FC; iconUrl?: string }) {
+function PopupHeader({ content, iconUrl }: { content: string | React.ReactElement; iconUrl?: string }) {
   return (
     <div className="popup-header--wrapper has-text-centered">
       {iconUrl && <img src={iconUrl} className="is-absolute popup-header--icon-img" />}
       <div className="popup-header--title-wrapper">
-        <Typography.Title level={2} className="popup-header--title">
-          {content}
-        </Typography.Title>
+        {typeof content === "string" ? (
+          <Typography.Title level={2} className="popup-header--title">
+            {content}
+          </Typography.Title>
+        ) : (
+          content
+        )}
       </div>
     </div>
   )
