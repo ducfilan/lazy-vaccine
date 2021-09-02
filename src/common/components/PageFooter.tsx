a2981a6202da7df320950020ac019811ba3cabd3
Duc Filan <ducfilan@gmail.com>
2021-09-02 18:02:56 +0800

    Feat: Add PageFooter component

---
+++ src/common/components/PageFooter.tsx
@@ -0,0 +1,8 @@
+import { Footer } from "antd/lib/layout/layout"
+import * as React from "react"
+
+function PageFooter() {
+  return <Footer className="page-footer" />
+}
+
+export default PageFooter
