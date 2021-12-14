d1d8f793bb66f004486bde1f617c3e0e8a82834d
Duc Filan <ducfilan@gmail.com>
2021-12-14 23:55:08 +0700

    Feat: Add mutation observer facade

---
+++ src/common/facades/mutationObserverFacade.ts
@@ -0,0 +1,39 @@
+export class MutationObserverFacade {
+  private observer: MutationObserver
+  private targetNode: Node | null
+  private config: MutationObserverInit
+
+  private DefaultConfig = {
+    childList: true,
+    subtree: true
+  }
+
+  constructor(selector: string, config: MutationObserverInit | null, callback: Function) {
+    this.targetNode = document.querySelector(selector)
+    this.config = config || this.DefaultConfig
+
+    this.observer = new MutationObserver(this._callbackForAddedNodes(callback))
+  }
+
+  private _callbackForAddedNodes(callback: Function) {
+    return function (mutations: MutationRecord[], observer: MutationObserver) {
+      for (var mutation of mutations) {
+        if (mutation.type == "childList") {
+          callback(mutation.addedNodes)
+        }
+      }
+    }
+  }
+
+  observe() {
+    if (!this.targetNode) {
+      throw new Error("wrong target node to observe")
+    }
+
+    this.observer.observe(this.targetNode, this.config)
+  }
+
+  stop() {
+    this.observer.disconnect()
+  }
+}
