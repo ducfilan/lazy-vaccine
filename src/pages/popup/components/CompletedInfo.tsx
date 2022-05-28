d7ce82f16c0b90c57937e139797a8da54668e264
nguyenson-2512 <62134526+nguyenson-2512@users.noreply.github.com>
2022-05-28 20:55:29 +0700

    fix completed-info ui (#18)

--- src/pages/popup/components/CompletedInfo.tsx
+++ src/pages/popup/components/CompletedInfo.tsx
@@ -14,15 +14,17 @@ import Icon, {
 
 import { User } from "@/common/types/types"
 import RegisterSteps from "@/common/consts/registerSteps"
-import { getSetsStatistics, updateUserInfo } from "@/common/repo/user"
+import { getSetsStatistics, getUserStatistics, updateUserInfo } from "@/common/repo/user"
 import { formatNumber } from "@/common/utils/stringUtils"
 
 import NextPrevButton from "./NextPrevButton"
 import PopupHeader from "./Header"
 import AvatarImage from "@/common/components/AvatarImage"
 import TreeIcon from "@img/ui/fa/tree-solid.svg"
-import { AppBasePath, AppPages } from "@/common/consts/constants"
+import { AchievementChartOrderIndex, AppBasePath, AppPages } from "@/common/consts/constants"
 import { useEffect, useState } from "react"
+import moment from "moment"
+import AchievementChart from "@/pages/app/Pages/user-profile/components/AchievementChart"
 
 const { Text } = Typography
 
@@ -79,22 +81,55 @@ function HeaderContent(props: { setsStatistics: any }) {
 function CompletedInfo() {
   const { user, setUser, http } = useGlobalContext()
   const [setsStatistics, setSetsStatistics] = useState<any>()
+  const [userStatistics, setUserStatistics] = useState<any>()
 
   useEffect(() => {
     user && getSetsStatisticsInfo()
   }, [http, user])
 
-  const getSetsStatisticsInfo = () => {
+  const getSetsStatisticsInfo = async () => {
     if (!http) return
-    getSetsStatistics(http)
-      .then(setSetsStatistics)
-      .catch(() => {
-        notification["error"]({
-          message: chrome.i18n.getMessage("error"),
-          description: chrome.i18n.getMessage("unexpected_error_message"),
-          duration: null,
-        })
+
+    try {
+      const endDate = Date.now()
+      const sevenDaysAgo: Date = new Date(endDate - 7 * 24 * 60 * 60 * 1000)
+      const [userStatistics, setStatistics] = await Promise.all([getUserStatistics(http, moment(sevenDaysAgo).format("YYYY-MM-DD"), moment(endDate).format("YYYY-MM-DD")), getSetsStatistics(http)])
+      let labels: string[] = [],
+        datasets: any = [
+          {
+            label: "Learnt items",
+            backgroundColor: "#36AE7C",
+            data: [],
+          },
+          {
+            label: "Incorrect items",
+            backgroundColor: "#187498",
+            data: [],
+          },
+          {
+            label: "Stared items",
+            backgroundColor: "#F9D923",
+            data: [],
+          },
+        ]
+      userStatistics.forEach((statistic) => {
+        if (!statistic) return
+
+        labels.push(moment(statistic.date).format("MM/DD/YYYY"))
+        datasets[AchievementChartOrderIndex.LearntItems].data.push(statistic.interactions.show || 0)
+        datasets[AchievementChartOrderIndex.IncorrectItems].data.push(statistic.interactions.incorrect || 0)
+        datasets[AchievementChartOrderIndex.StaredItems].data.push(statistic.interactions.star || 0)
       })
+
+      setUserStatistics({ labels, datasets })
+      setSetsStatistics(setStatistics)
+    } catch (error) {
+      notification["error"]({
+        message: chrome.i18n.getMessage("error"),
+        description: chrome.i18n.getMessage("unexpected_error_message"),
+        duration: null,
+      })
+    }
   }
 
   const goBack = async () => {
@@ -144,11 +179,6 @@ function CompletedInfo() {
               </Button>
             </Col>
             <Col span={12}>
-              <Button className="completed-info--button-full-stats" size="large" icon={<BarChartOutlined />} block>
-                {chrome.i18n.getMessage("popup_stats_full_stats")}
-              </Button>
-            </Col>
-            <Col span={12}>
               <Button
                 className="completed-info--button-my-profile"
                 size="large"
@@ -160,7 +190,13 @@ function CompletedInfo() {
                 {chrome.i18n.getMessage("popup_stats_my_profile")}
               </Button>
             </Col>
-            <Col span={12}>
+            {/* TODO: update full-stats, setting */}
+            <Col span={12} style={{ display: "none" }}>
+              <Button className="completed-info--button-full-stats" size="large" icon={<BarChartOutlined />} block>
+                {chrome.i18n.getMessage("popup_stats_full_stats")}
+              </Button>
+            </Col>
+            <Col span={12} style={{ display: "none" }}>
               <Button className="completed-info--button-settings" size="large" icon={<SettingOutlined />} block>
                 {chrome.i18n.getMessage("popup_stats_settings")}
               </Button>
@@ -168,6 +204,11 @@ function CompletedInfo() {
           </Row>
         </div>
       </>
+      }
+      {
+        userStatistics && <div className="chart-wrapper">
+          <AchievementChart statistics={userStatistics} />
+        </div>
       }
     </div>
   )
